efd2abfd43db904285733d5862f86e26f438ccc1
shaoshitong <1090784053@qq.com>
2022-04-16 23:09:40 +0800

    3080ti change

--- SST/loss/Policyloss.py
+++ SST/loss/Policyloss.py
@@ -6,11 +6,18 @@ from torchdistill.losses.single import register_single_loss
 
 @register_single_loss
 class AuxPolicyKDLoss(nn.CrossEntropyLoss):
-    def __init__(self, module_path='policy_module', module_io='output', reduction='mean',feature_nums=128,policy_nums=7,**kwargs):
+    def __init__(self, module_path='policy_module', module_io='output', reduction='mean',feature_nums=128,policy_nums=7,
+                 type="mse",**kwargs):
         super().__init__(reduction=reduction, **kwargs)
         self.module_path = module_path
         self.module_io = module_io
-        self.linear=nn.Linear(feature_nums,policy_nums+2).cuda() # policy+classes+identity
+        if type=="mse":
+            self.linear=nn.Linear(feature_nums,policy_nums+2).cuda() # policy+classes+identity
+            self.mse=nn.MSELoss(reduction='sum')
+        else:
+            self.linear=nn.Linear(feature_nums,2*(policy_nums+2)).cuda() # policy+classes+identity
+        self.type=type
+
 
     def forward(self, student_io_dict, teacher_io_dict, target,*args, **kwargs):
         policy_module_outputs = teacher_io_dict[self.module_path][self.module_io]
@@ -27,16 +34,24 @@ class AuxPolicyKDLoss(nn.CrossEntropyLoss):
         target=target.view(-1,policy_len+1)
         b1_target=target[b1_indices]
         b2_target=target[b2_indices]
-        print(b1_target,b2_target)
         b1_target=b1_target.unsqueeze(-1).expand(-1,-1,b1).transpose(0,2)
         b2_target=b2_target.unsqueeze(-1).expand(-1,-1,b2)
         target_matrix=(b1_target==b2_target).float()
         target_matrix=torch.cat([torch.eye(target_matrix.shape[0]).to(target_matrix.device).unsqueeze(1),target_matrix],1)
-        b1_output = b1_output.unsqueeze(-1).expand(-1,-1,b1).transpose(0,2)
-        b2_output = b2_output.unsqueeze(-1).expand(-1,-1,b2)
-        output_matrix=torch.cat([b1_output,b2_output],1)
-        learning_matrix=self.linear(output_matrix.transpose(1,2)).transpose(1,2)
-        return super().forward(cos_similarities, targets)
+        if self.type=="mse":
+            b1_output = b1_output.unsqueeze(-1).expand(-1, -1, b1).transpose(0, 2)
+            b2_output = b2_output.unsqueeze(-1).expand(-1, -1, b2)
+            output_matrix = torch.cat([b1_output, b2_output], 1)
+            learning_matrix = self.linear(output_matrix.transpose(1, 2)).transpose(1, 2)
+            return self.mse(learning_matrix,target_matrix)
+        else:
+            b1_output = b1_output.unsqueeze(-1).expand(-1, -1, b1).transpose(0, 2)
+            b2_output = b2_output.unsqueeze(-1).expand(-1, -1, b2)
+            output_matrix = torch.cat([b1_output, b2_output], 1)
+            learning_matrix = self.linear(output_matrix.transpose(1, 2)).transpose(1, 2)
+            target_matrix=target_matrix.transpose(1,2).view(-1).int()
+            learning_matrix=learning_matrix.transpose(1,2).view(-1,2)
+            return super().forward(learning_matrix,target_matrix)
 
 
 @register_single_loss
